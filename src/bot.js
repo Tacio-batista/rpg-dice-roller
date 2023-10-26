973e9b4525abc0e6e54dcd1ccbeeaf4f80213317
Tacio-batista <thaciobb@gmail.com>
2023-10-26 20:03:36 -0300

    tentando apagar o menu de regras

--- src/bot.js
+++ src/bot.js
@@ -42,8 +42,13 @@ bot.command("erro", async (ctx) => {
 });
 
 bot.command("regras", async (ctx) =>{
-  await ctx.reply("Regras!", { reply_markup: { inline_keyboard: [[{text: "Gerais!", url: links.rules.general },{text: "Combate!", url: links.rules.combat },{text: "Magias!", url: links.rules.spells }]] } });
+  await ctx.reply("Regras!", { reply_markup: { inline_keyboard: [[{text: "Gerais!", url: links.rules.general }],[{text: "Combate!", url: links.rules.combat }],[{text: "Magias!", url: links.rules.spells }],[text: "❎"]] } });
 });
+
+
+bot.callbackQuery("❎", ctx => {
+  bot.api.deleteMessage(chat_id: ctx.from.chat_id, message_id: ctx.message.message_id);
+})
 
 
 bot.api.setMyCommands([
