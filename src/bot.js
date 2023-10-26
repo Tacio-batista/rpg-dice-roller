e67d0139f9591ac47286ae8dff79689a89255518
Tacio-batista <thaciobb@gmail.com>
2023-10-26 20:15:56 -0300

    agora com um x

--- src/bot.js
+++ src/bot.js
@@ -42,11 +42,11 @@ bot.command("erro", async (ctx) => {
 });
 
 bot.command("regras", async (ctx) =>{
-  await ctx.reply("Regras!", { reply_markup: { inline_keyboard: [[{text: "Gerais!", url: links.rules.general }],[{text: "Combate!", url: links.rules.combat }],[{text: "Magias!", url: links.rules.spells }],[{text: "Fechar", url: "http//:www.google.com"}]] } });
+  await ctx.reply("Regras!", { reply_markup: { inline_keyboard: [[{text: "Gerais!", url: links.rules.general }],[{text: "Combate!", url: links.rules.combat }],[{text: "Magias!", url: links.rules.spells }],[{text: "❎", "au"}]] } });
 });
 
 
-bot.callbackQuery("Fechar", ctx => {
+bot.callbackQuery("au", ctx => {
   bot.api.deleteMessage(chat_id: ctx.from.chat_id, message_id: ctx.message.message_id);
 })
 
