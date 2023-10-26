3dc52513bf42130fc532c5477201f0b0cf555ce6
Tacio-batista <thaciobb@gmail.com>
2023-10-26 19:19:52 -0300

    menu regras pronto

--- src/menus/index.js
+++ src/menus/index.js
@@ -4,11 +4,11 @@ const { playersID, links } = require("../constants/characters");
 
 
 const rulesMenu = new Menu("rules-menu")
-  .webApp(links.rules.general)
+  .webApp("Gerais!", links.rules.general)
   .row()
-  .webApp(links.rules.combat)
+  .webApp("Combate!",links.rules.combat)
   .row()
-  .webApp(links.rules.spells);
+  .webApp("Magias!", links.rules.spells);
 
 module.exports = {
   rulesMenu
