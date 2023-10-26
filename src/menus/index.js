8b652d1b73d8d85907837e4ece425a096c6c5ee2
Tacio-batista <thaciobb@gmail.com>
2023-10-26 19:27:18 -0300

    webapp no menu ta errado

--- src/menus/index.js
+++ src/menus/index.js
@@ -4,9 +4,9 @@ const { playersID, links } = require("../constants/characters");
 
 
 const rulesMenu = new Menu("rules-menu")
-  .webApp("Gerais!", {url: links.rules.general})
-  .webApp("Combate!", {url: links.rules.combat})
-  .webApp("Magias!", {url: links.rules.spells});
+  .webApp("Gerais!", url: links.rules.general)
+  .webApp("Combate!", url: links.rules.combat)
+  .webApp("Magias!", url: links.rules.spells);
 
 module.exports = {
   rulesMenu
