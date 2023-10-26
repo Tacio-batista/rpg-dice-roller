ea784134f4f5876f6d01397a8fe71ff6ea1f9c14
Tacio-batista <thaciobb@gmail.com>
2023-10-26 20:15:01 -0300

    tentando apagar o menu de regras

--- src/bot.js
+++ src/bot.js
@@ -42,7 +42,7 @@ bot.command("erro", async (ctx) => {
 });
 
 bot.command("regras", async (ctx) =>{
-  await ctx.reply("Regras!", { reply_markup: { inline_keyboard: [[{text: "Gerais!", url: links.rules.general }],[{text: "Combate!", url: links.rules.combat }],[{text: "Magias!", url: links.rules.spells }],[{text: "Fechar", url: ""}]] } });
+  await ctx.reply("Regras!", { reply_markup: { inline_keyboard: [[{text: "Gerais!", url: links.rules.general }],[{text: "Combate!", url: links.rules.combat }],[{text: "Magias!", url: links.rules.spells }],[{text: "Fechar", url: "http//:www.google.com"}]] } });
 });
 
 
