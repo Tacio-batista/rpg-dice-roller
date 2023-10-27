a9a4955e65d9edb5d1e11c01be8c1e3ecb730db3
Tacio-batista <thaciobb@gmail.com>
2023-10-27 10:28:55 -0300

    aceitando modificadores

--- src/bot.js
+++ src/bot.js
@@ -23,10 +23,10 @@ bot.use(session({ initial: () => ({}) }));
 // bot.use(hydrateReply);
 
 bot.command(["r", "roll", "rolar"], async (ctx) => {
-  if(ctx.match?){
+  if(ctx.match !== ""){
   const result = await rollDice(ctx.match);
   const playerName = await selectName(ctx);
-  await ctx.reply(`${playerName} rolou${result.text}`,{reply_to_message_id: ctx.message.message_id});}
+  await ctx.reply(${result.total !== false ? `${playerName} rolou${result.text}`: result.text},{reply_to_message_id: ctx.message.message_id});}
 });
 
 bot.command("fulminante", async (ctx) => {
@@ -39,7 +39,7 @@ bot.command("erro", async (ctx) => {
   const result = await rollDice("3d6");
   const output = await erroCritico(result.total);
   const playerName = await selectName(ctx);
-  await ctx.reply(${result.total !== false ?`${playerName} rolou${result.text}\n\nE o resultado do ERRO CRÍTICO foi:\n\n${output}`: result.text},{reply_to_message_id: ctx.message.message_id});
+  await ctx.reply(`${playerName} rolou${result.text}\n\nE o resultado do ERRO CRÍTICO foi:\n\n${output}`,{reply_to_message_id: ctx.message.message_id});
 });
 
 bot.command("regras", async (ctx) =>{
