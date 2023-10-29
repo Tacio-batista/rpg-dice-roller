8ab16f577fcaf31ad97bc2e439f4c3b0ed6f1d2c
Tacio-batista <48337041+Tacio-batista@users.noreply.github.com>
2023-10-29 10:48:27 -0300

    Update index.js

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -76,7 +76,7 @@ function getDescForType(typeResult, type){
       default:
         typeDesc = body.part.find(part => part.name === typeResult);
     }
-  if(type === "centauro" && typeResult === "Corpo"){
+  if(type === "centauro" && typeResult === "Tronco"){
     typeDesc.desc += "\n\n - Para centauro 9–10 significa que a parte animal foi atingida, enquanto 11 significa que a parte superior humanoide foi atingida."
   }
   return typeDesc;
