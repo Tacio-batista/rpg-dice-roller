a0a8ba678654cb0dcd6d18b1d4a735b0b23cdf7d
tacio-batista <tacio-batista@github.com>
2024-03-02 20:03:17 -0300

    tratamento de dados de rolagem completo

--- src/bot.js
+++ src/bot.js
@@ -10,13 +10,13 @@ const { bold, fmt, hydrateReply, italic, link, parseMode } = require(
 );
 
 
-const {
-    BOT_API_TOKEN: token = '',
-} = process.env;
+// const {
+//     BOT_API_TOKEN: token = '',
+// } = process.env;
 
-// require("dotenv").config();
+require("dotenv").config();
 
-// const token = process.env.BOT_API_TOKEN || "";
+const token = process.env.BOT_API_TOKEN || "";
 // Defina seu token nas variáveis de ambiente do Vercel
 const bot = new Bot(token);
 
@@ -133,61 +133,79 @@ bot.command("tipo", async (ctx) =>{
   }
   
 });
-// bot.command("teste", async (ctx) => {
-//   await deleteP(9);
-//   if(ctx.match !== ""){
-//   // Inicializar a lista de divisão
-//   const str = ctx.match;
-//   let divisao = [];
-//   // Inicializar o valor e o sinal atuais
-//   let valor = [];
-//   let sinal = "";
-//   // Percorrer a string da esquerda para a direita
-//   for (let i = 0; i < str.length; i++){
-//     // Se o caractere for d, ignorar
-//     let c = str[i];
-//     if (c == "d") {
-//       if(!/^\d$/.test(str[i-1])){
-//         valor.push(1);
-//       }
-//       if(!/^\d$/.test(str[i+1])){
-//         valor.push(0);
-//       }
-//       continue;
-//     }else{
-//       for(let u = i+1; u < str.length; u++){
-//         if(str[u] !== "d" && /^\d$/.test(str[u]) && /^\d$/.test(str[u-1])){
-//           c +=str[u]; 
-//         }else{
-//           i = u-1;
-//           break;
-//         }
-//         if(u === str.length - 1){
-//           i = u;
-//         }
-//       }
-//     }
-//     // Se o caractere for uma letra maiúscula, adicionar ao valor atual
+bot.command("teste", async (ctx) => {
+  await deleteP(9);
+  if(ctx.match !== ""){
+  // Inicializar a lista de divisão
+  const str = ctx.match;
+  let divisao = [];
+  // Inicializar o valor e o sinal atuais
+  let valor = [];
+  let sinal = "";
+  // Percorrer a string da esquerda para a direita
+  for (let i = 0; i < str.length; i++){
+    // Se o caractere for d, ignorar
+    let c = str[i];
+    if (c == "d") {
+      if(!/^\d$/.test(str[i-1])){
+        valor.push(1);
+      }
+      if(!/^\d$/.test(str[i+1])){
+        valor.push(0);
+      }
+      continue;
+    }else{
+      for(let u = i+1; u < str.length; u++){
+        if(str[u] !== "d" && /^\d$/.test(str[u]) && /^\d$/.test(str[u-1])){
+          c +=str[u]; 
+        }else{
+          i = u-1;
+          break;
+        }
+        if(u === str.length - 1){
+          i = u;
+        }
+      }
+    }
+    // Se o caractere for uma letra maiúscula, adicionar ao valor atual
 
-//     if (!isNaN(c)) {
-//       valor.push(parseInt(c));
-//     }else if (c === "+" || c === "-" || c === "*") {
-//       console.log(valor);
-//       if (valor.length === 0 ) {
-//         valor.push(0);
-//       }
-//       divisao.push({valor: valor, sinal: sinal});
-//       valor = [];
-//       sinal = c;
-//     }
-//     }
+    if (!isNaN(c)) {
+      valor.push(parseInt(c));
+    }else if (c === "+" || c === "-" || c === "*") {
+      if (valor.length === 0 ) {
+        valor.push(0);
+      }
+      divisao.push({valor: valor, sinal: sinal});
+      valor = [];
+      sinal = c;
+    }
+    }
   
-//   divisao.push({valor: valor, sinal: sinal});
-//   divisao[0].sinal = "+";
-//   divisao = divisao.filter(objeto => !objeto.valor.includes(0));
-//     console.log(divisao);
-//   }
-// });
+  divisao.push({valor: valor, sinal: sinal});
+  divisao[0].sinal = "+";
+  divisao = divisao.filter(objeto => !objeto.valor.includes(0));
+  const index = divisao.findIndex(obj => obj.sinal === '*' && obj.valor.length === 1);
+
+  if (index !== -1) {
+    const objeto = divisao.splice(index, 1)[0];
+
+    // Adicione o objeto removido de volta ao final da array
+    divisao.push(objeto);
+    console.log(objeto)
+    
+    // Remova quaisquer outros objetos '*' da matriz
+    for (let i = divisao.length - 2; i >= 0; i--) {
+      // console.log(divisao[i]);
+      if (divisao[i].sinal === '*') {
+
+        divisao.splice(i, 1);
+      }
+    }
+  }
+
+  console.log(divisao);
+  }
+});
 
 bot.api.setMyCommands([
   { command: "roll", description: "Use o formato XdY[+/-Z][*W] [texto]" },
@@ -200,5 +218,5 @@ bot.api.setMyCommands([
   { command: "ficha", description: "Dispõe a ficha do seu personagem" },
   { command: "help", description: "Lista de comandos explicados" }
 ]);
-// bot.start();
+bot.start();
 module.exports = { bot };
